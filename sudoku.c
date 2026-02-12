f9f64f39d377e8891c36d007dbf40dba11ae60d0
subcomp24 <166525529+subcomp24@users.noreply.github.com>
2026-02-12 13:56:29 +0545

    Valid move check

--- sudoku.c
+++ sudoku.c
@@ -1,25 +1,57 @@
 #include<stdio.h>
 int puzzle[9][9] = {
     {3,0,0,0,2,0,0,7,0},
-    {0,0,0,6,0,0,0,0,0},
-    {0,9,8,0,0,0,0,6,0},
-    {8,0,0,0,6,0,0,0,3},
-    {4,0,0,8,0,3,0,0,1},
-    {7,0,0,0,2,0,0,0,6},
-    {0,6,0,0,0,0,2,8,0},
-    {0,0,0,4,1,9,0,0,5},
-    {0,7,0,0,8,0,0,1,7},
+    {9,0,0,5,0,0,0,1,4},
+    {0,1,6,3,7,0,0,0,8},
+    {2,0,0,8,0,0,0,0,1},
+    {5,0,0,0,4,1,8,0,0},
+    {0,8,9,0,0,0,0,5,0},
+    {0,0,5,0,1,0,2,8,0},
+    {0,4,0,0,0,6,0,9,3},
+    {7,3,1,0,8,2,0,0,0},
 
 };
 
 void printPuzzle(int puzzle[9][9]);
-
+int validMove(int puzzle[9][9],int row, int column, int number);
 
 int main (){
     printPuzzle(puzzle);
 
     return 0;
 
+}
+
+
+
+int validMove(int puzzle[9][9],int row,int column,int number){
+
+    // row check
+    for(int i=0;i<9;i++){
+        if (puzzle[row][i]==number){
+            return 0 ;
+        }
+    }
+
+    // column check
+    for(int i=0;i<9;i++){
+        if (puzzle[i][column]==number){
+            return 0 ;
+        }
+    }
+
+    int r=row-row%3;
+    int c=column-column%3;
+
+    for (int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+            if(puzzle[r+i][c+j]==number){
+                return 0;
+            }
+        }
+    }
+
+    return 1;
 }
 
 void printPuzzle(int puzzle[9][9]){
